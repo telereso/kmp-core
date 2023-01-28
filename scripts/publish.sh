0db568d9c8378ae99339d7a4a19001092deed3dd
Ahmed Alnaami <ahmedalnaami@airasia.com>
2023-01-28 11:55:32 +0800

    [CHORE] Publish Pages

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,6 +6,7 @@ if [[ "$@" == *"--sonatype" ]]; then
     ./gradlew preBuild koverHtmlReport dokkaHtml publishAllPublicationsToSonatypeRepository "-PpublishVersion=$PUBLISH_VERSION"
 #    ./gradlew closeAndReleaseSonatypeStagingRepository
 else
-    ./gradlew preBuild koverHtmlReport dokkaHtml -Dmaven.repo.local="$(pwd)/build/.m2/repository" publishToMavenLocal "-PpublishVersion=$PUBLISH_VERSION"
+#    ./gradlew preBuild koverHtmlReport dokkaHtml -Dmaven.repo.local="$(pwd)/build/.m2/repository" publishToMavenLocal "-PpublishVersion=$PUBLISH_VERSION"
+    ./gradlew preBuild koverHtmlReport dokkaHtml
 fi
 
