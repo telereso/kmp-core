401c1d4666bb04ff62a4d50c6758011bd8267189
Ahmed na <ahmed.alnaami92@gmail.com>
2023-06-28 14:58:22 +0800

    [CHORE] Update kotlin to 1.8.21 (#29)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,12 @@ plugins {
     id("com.android.application").version("7.3.1").apply(false)
     id("com.android.library").version("7.3.1").apply(false)
 
-    id("org.jetbrains.kotlin.android").version("1.7.21").apply(false)
-    id ("org.jetbrains.kotlin.plugin.parcelize").version("1.7.20").apply(false)
-    kotlin("multiplatform").version("1.7.21").apply(false)
-    id("org.jetbrains.kotlin.native.cocoapods").version("1.7.22").apply(false)
+    id("org.jetbrains.kotlin.android").version("1.8.21").apply(false)
+    id ("org.jetbrains.kotlin.plugin.parcelize").version("1.8.21").apply(false)
+    kotlin("multiplatform").version("1.8.21").apply(false)
+    id("org.jetbrains.kotlin.native.cocoapods").version("1.8.21").apply(false)
 
-    id("com.squareup.sqldelight").version("1.5.3").apply(false)
-    id("io.telereso.kmp").version("0.0.23").apply(false)
+    id("com.squareup.sqldelight").version("1.5.5").apply(false)
 }
 
 
@@ -31,9 +30,9 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.squareup.sqldelight:gradle-plugin:1.5.4")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.21")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
+        classpath("com.squareup.sqldelight:gradle-plugin:1.5.5")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.8.21")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.8.20")
         classpath("org.jfrog.buildinfo:build-info-extractor-gradle:latest.release")
     }
     // extra.apply {
@@ -49,7 +48,7 @@ val targetSdkVersion by extra { 31 }
 
 // Dependencies versions
 val ktorVersion by extra { "2.1.3" }
-val sqlDelightVersion by extra { "1.5.4" }
+val sqlDelightVersion by extra { "1.5.5" }
 val coroutinesVersion by extra { "1.6.4" }
 val napierVersion by extra { "2.6.1" }
 val multiplatformSettingsVersion by extra { "1.0.0-RC" }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,26 @@
 pluginManagement {
+    val localProps = mutableMapOf<String, String>()
+    File("local.properties").apply {
+        if (exists()) useLines { lines ->
+            lines.forEach {
+                val parts = it.split("=", limit = 2)
+                if (parts.size == 2) localProps[parts[0].trim()] = parts[1].trim()
+            }
+        }
+    }
+
+    val teleresoKmpVersion: String by settings
+    val kmpVersion = localProps.getOrDefault("teleresoKmpVersion", teleresoKmpVersion)
+    plugins {
+        id("io.telereso.kmp") version kmpVersion apply false
+    }
     repositories {
         google()
         gradlePluginPortal()
         mavenCentral()
         maven { url = uri("https://repo.spring.io/milestone") }
         maven { url = uri("https://repo.spring.io/snapshot") }
+        maven { url = uri("https://s01.oss.sonatype.org/content/groups/staging") }
     }
 }
 
@@ -12,6 +28,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://s01.oss.sonatype.org/content/groups/staging") }
     }
 }
 
