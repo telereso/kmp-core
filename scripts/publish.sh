5c4760548036d614bbb322a3cf8c799245b53101
Ahmed Alnaami <ahmedalnaami@airasia.com>
2024-09-15 09:56:35 +0800

    [CHORE] kotlin 2.0

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,14 +4,14 @@ PUBLISH_VERSION=${1:-0.0.1}
 
 if [[ "$@" == *"--sonatype" ]]; then
     ./gradlew koverXmlReport koverHtmlReport koverPrintCoverage dokkaHtml publishAllPublicationsToSonatypeRepository "-PpublishVersion=$PUBLISH_VERSION" \
-    -x catalog:publishToMavenLocal
+    -x catalog:publishToMavenLocal --no-configuration-cache
 #    ./gradlew closeAndReleaseSonatypeStagingRepository
 elif [[ "$@" == *"--local" ]]; then
     ./gradlew koverXmlReport koverHtmlReport koverPrintCoverage  dokkaHtml -Dmaven.repo.local="$(pwd)/build/.m2/repository" publishToMavenLocal "-PpublishVersion=$PUBLISH_VERSION" \
-    -x catalog:publishToMavenLocal
+    -x catalog:publishToMavenLocal --no-configuration-cache
 else
     ./gradlew publishToMavenLocal "-PpublishVersion=$PUBLISH_VERSION-local" \
-    -x catalog:publishToMavenLocal
+    -x catalog:publishToMavenLocal --no-configuration-cache
     echo "published $PUBLISH_VERSION-local !"
 fi
 
